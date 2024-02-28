6a5f757f80f50084cee9624ad874a8509788f078
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-02-28 21:56:51 +0000

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,13 +8,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    /* If node has no children, it's a leaf */
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    /* Recursively count the leaves in the left and right subtrees */
-    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 }
