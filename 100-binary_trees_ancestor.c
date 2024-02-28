63bb9bfae08f9b63f079e5a2f480f076f125ca53
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-02-28 22:26:11 +0000

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,1 +1,33 @@
-100-binary_trees_ancestor.c
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
+ *
+ * Return: Pointer to the lowest common ancestor, or NULL if no common
+ * ancestor was found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+                                     const binary_tree_t *second)
+{
+    const binary_tree_t *temp;
+
+    if (first == NULL || second == NULL)
+        return (NULL);
+
+    temp = second;
+    while (first)
+    {
+        while (second)
+        {
+            if (first == second)
+                return ((binary_tree_t *)first);
+            second = second->parent;
+        }
+        second = temp;
+        first = first->parent;
+    }
+
+    return (NULL);
+}
