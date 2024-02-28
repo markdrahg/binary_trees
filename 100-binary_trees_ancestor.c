d0111b5566de80d81dab7435beea4fe1dd4b9f87
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-02-28 22:42:38 +0000

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,7 +9,7 @@
  * ancestor was found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+				const binary_tree_t *second)
 {
 	const binary_tree_t *temp;
 
