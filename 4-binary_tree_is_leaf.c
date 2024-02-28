0c83edffb8078b93fe14cc6b4fcad5072c0e4626
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-02-28 20:30:07 +0000

    Update 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -23,8 +23,8 @@ int binary_tree_is_root(const binary_tree_t *node)
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
- 	if (node == NULL)
- 		return (0);
+	if (node == NULL)
+		return (0);
 
 	return (node->left == NULL && node->right == NULL);
 }
