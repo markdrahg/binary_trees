9f63c05b658b9743280cb3e4e3077ee29bf020a1
markdrahg <neutral520@gmail.com>
2024-02-28 12:27:26 -0800

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,10 +8,10 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
+	if (node == NULL)
+		return (0);
 
-    return (node->parent == NULL);
+	return (node->parent == NULL);
 }
 
 
@@ -23,8 +23,8 @@ int binary_tree_is_root(const binary_tree_t *node)
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
+ 	if (node == NULL)
+ 		return (0);
 
-    return (node->left == NULL && node->right == NULL);
+	return (node->left == NULL && node->right == NULL);
 }
