0c609b572d1715a8fc16a2b938e76a1ce6dd2dfb
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-02-28 23:01:45 +0000

    Update 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,38 +1,65 @@
 #include "binary_trees.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * binary_tree_levelorder - Traverses a binary tree using LTO
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node
+ * binary_tree_levelorder - Goes through a binary tree using level-order traversal
+ * @tree: A pointer to the root node of the tree to traverse
+ * @func: A pointer to a function to call for each node
+ *
+ * Description: If tree or func is NULL, do nothing
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL)
         return;
-  
-    const binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 10000);
+
+    /* Create a queue for level order traversal */
+    binary_tree_t **queue = malloc(sizeof(binary_tree_t *));
     if (queue == NULL)
         return;
 
-    int front = 0, rear = -1;
-    const binary_tree_t *curr = tree;
+    int front = 0, rear = -1, size = 1;
+    const binary_tree_t *current;
 
-    while (curr != NULL)
+    /* Enqueue root */
+    queue[++rear] = (binary_tree_t *)tree;
+
+    while (front <= rear)
     {
-        func(curr->n);
+        /* Dequeue node and make it current */
+        current = queue[front++];
 
-        if (curr->left != NULL)
-            queue[++rear] = curr->left;
+        /* Call the function */
+        func(current->n);
 
-        if (curr->right != NULL)
-            queue[++rear] = curr->right;
-      
-        if (front <= rear)
-            curr = queue[front++];
-        else
-            curr = NULL;
+        /* Enqueue left child */
+        if (current->left)
+        {
+            if (rear >= size - 1)
+            {
+                size *= 2;
+                queue = realloc(queue, sizeof(binary_tree_t *) * size);
+                if (queue == NULL)
+                    return;
+            }
+            queue[++rear] = current->left;
+        }
+
+        /* Enqueue right child */
+        if (current->right)
+        {
+            if (rear >= size - 1)
+            {
+                size *= 2;
+                queue = realloc(queue, sizeof(binary_tree_t *) * size);
+                if (queue == NULL)
+                    return;
+            }
+            queue[++rear] = current->right;
+        }
     }
 
+    /* Free the queue */
     free(queue);
 }
